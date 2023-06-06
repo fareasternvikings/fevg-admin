cc01ad16ea17a5ab73a94a6baab5df63620d2d0f
a.mayatskiy <work.mayatskiy@gmail.com>
2023-06-07 05:35:42 +1000

    fix: volumes

--- config/plugins.js
+++ config/plugins.js
@@ -39,10 +39,26 @@ module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: 'local',
-      providerOptions: {
-        sizeLimit: 1000000,
-      },
+      // providerOptions: {
+      //   sizeLimit: 1000000,
+      // },
     },
   },
+
+  // upload: {
+  //   config: {
+  //     provider: 'cloudinary',
+  //     providerOptions: {
+  //       cloud_name: env('CLOUDINARY_NAME'),
+  //       api_key: env('CLOUDINARY_KEY'),
+  //       api_secret: env('CLOUDINARY_SECRET'),
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
   //...
 });
