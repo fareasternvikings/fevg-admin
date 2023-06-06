942f44d086331afbf2e9985a7660880712803ab9
a.mayatskiy <work.mayatskiy@gmail.com>
2023-06-06 13:16:54 +1000

    feat: add cache plugin

--- config/plugins.js
+++ config/plugins.js
@@ -3,5 +3,25 @@ module.exports = ({ env }) => ({
   'import-export-entries': {
     enabled: true,
   },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 3600,
+        },
+      },
+      strategy: {
+        contentTypes: [
+          // list of Content-Types UID to cache
+          // "api::category.category",
+          // "api::article.article",
+          // "api::global.global",
+          // "api::homepage.homepage",
+        ],
+      },
+    },
+  },
   //...
 });
