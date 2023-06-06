a6fe96e2ab8f2444e182cda153c7ae1f5ecf41ec
a.mayatskiy <work.mayatskiy@gmail.com>
2023-06-06 14:10:28 +1000

    feat: add sentry plugin

--- config/plugins.js
+++ config/plugins.js
@@ -28,5 +28,12 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  sentry: {
+    enabled: true,
+    config: {
+      dsn: env('SENTRY_DSN'),
+      sendMetadata: true,
+    },
+  },
   //...
 });
