d9b3d8011e910384d0c11f482afabfc1cf16fcc8
a.mayatskiy <work.mayatskiy@gmail.com>
2023-06-07 16:20:44 +1000

    feat: add cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -36,26 +36,25 @@ module.exports = ({ env }) => ({
   //   },
   // },
 
-  upload: {
-    config: {
-      provider: 'local',
-    },
-  },
-
   // upload: {
   //   config: {
-  //     provider: 'cloudinary',
-  //     providerOptions: {
-  //       cloud_name: env('CLOUDINARY_NAME'),
-  //       api_key: env('CLOUDINARY_KEY'),
-  //       api_secret: env('CLOUDINARY_SECRET'),
-  //     },
-  //     actionOptions: {
-  //       upload: {},
-  //       uploadStream: {},
-  //       delete: {},
-  //     },
+  //     provider: 'local',
   //   },
   // },
-  //...
+
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
